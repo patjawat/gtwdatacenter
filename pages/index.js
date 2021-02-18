d2a1167ce722751fb97be7d972aa94944af05986
patjawat <patjawat@gmail.com>
2021-02-18 14:16:20 +0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Theme from "../themes";
-import Authlog from "../components/authlog"
 
 import TotalSummary from "../components/totalSummary"
 import OrderList from "../components/card-orderlist"
