b77253b4f9e1496457494ae3b6aaf5a199f63011
patjawat <patjawat@gmail.com>
2021-02-18 13:29:36 +0700

    update

--- components/chartSuumary.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function chartSuumary() {
-    return (
-        <div>
-            chard
-        </div>
-    )
-}
--- next.config.js
+++ next.config.js
@@ -5,18 +5,13 @@ const resultOs = detector.parseOs(userAgent);
 const resultClient = detector.parseClient(userAgent);
 const resultDeviceType = detector.parseDeviceType(userAgent, resultOs, resultClient, {});
 const result = Object.assign({os:resultOs}, {client:resultClient}, {device: resultDeviceType});
-// console.log('Result parse lite', result);
-// console.log('Result parse lite', result);
 
 module.exports = {
   distDir: 'build',
   // basePath: '/datacenterv2',
-  // basePath: '',
     env: {
       customKey: 'my-value',
-      // api:'http://192.168.1.165:8001/api/',
       // api:'http://127.0.0.1:8001/api/',
-      // api:'http://43.229.149.13:81/gtwdatacenter-api/api/',
       api:'http://27.254.191.157:81/gtwdatacenter-api/api/',
       device:result,
     },
